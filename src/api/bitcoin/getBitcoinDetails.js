04b1e3484d5702a555c20b07d5244f134d08ace2
ronnie <ronnie@freehitnews.com>
2021-02-22 11:32:51 +0530

    added token

--- src/api/bitcoin/getBitcoinDetails.js
+++ src/api/bitcoin/getBitcoinDetails.js
@@ -1,6 +1,6 @@
 const getBitcoinDetails = async (address) => {
   const response = await fetch(
-    `https://api.blockcypher.com/v1/btc/test3/addrs/${address}/full?limit=50`,
+    `https://api.blockcypher.com/v1/btc/test3/addrs/${address}/full?limit=50&token=a657369943a148eba820eb667fcd5c26`,
   );
 
   const data = await response.json();
